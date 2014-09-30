b013b1d28b2c04a5b09f0e06f27fa46164e4b500
Sergey Kuzhavskiy <kuzh@rademade.com>
2014-09-30 18:26:21 +0300

    added setter

---
+++ lib/mongoid_sortable_relation/accessors.rb
@@ -0,0 +1,42 @@
+module Mongoid
+  module Relations
+    module Accessors
+      module ClassMethods
+
+        def setter(name, metadata)
+          re_define_method("#{name}=") do |object|
+            without_autobuild do
+              sort_related_items(object, name, metadata) if metadata.sortable?
+              if value = get_relation(name, metadata, object)
+                set_relation(name, value.substitute(object.substitutable))
+              else
+                __build__(name, object.substitutable, metadata)
+              end
+            end
+          end
+          self
+        end
+
+        private
+
+        def sort_related_items(object, name, metadata)
+          nullify_related_items(send(name))
+          update_sortable_field(object, metadata) if metadata.relation.macro == :has_many
+          object.each &:reload
+        end
+
+        def nullify_related_items(related_items)
+          related_items.nullify unless related_items.empty?
+        end
+
+        def update_sortable_field(object, metadata)
+          object.each_with_index do |entity, index|
+            entity.send(:"#{metadata.sortable_field}=", index + 1)
+            entity.save
+          end
+        end
+
+      end
+    end
+  end
+end
--- spec/models/many_to_many_spec.rb
+++ spec/models/many_to_many_spec.rb
@@ -6,18 +6,28 @@ describe 'Many to many sortable relation' do
     Post.create
   end
 
-  describe 'get relations' do
+  let!(:tags) do
+    tag1 = Tag.create :name => 'Name1'
+    tag2 = Tag.create :name => 'Name2'
+    tag3 = Tag.create :name => 'Name3'
+    post.tags = [tag2, tag1, tag3]
+    post.save
+  end
 
-    let!(:tags) do
-      tag1 = Tag.create :name => 'Name1'
-      tag2 = Tag.create :name => 'Name2'
-      tag3 = Tag.create :name => 'Name3'
-      post.tags = [tag2, tag1, tag3]
-      post.save
-    end
+  describe '.getter' do
 
     it 'returns ordered relations' do
       expect(post.tags.map(&:name)).to eq(%w(Name2 Name1 Name3))
+    end
+
+  end
+
+  describe '.setter' do
+
+    it 'updates order' do
+      tags = [Tag.find_by(:name => 'Name1'), Tag.find_by(:name => 'Name3'), Tag.find_by(:name => 'Name2')]
+      post.tags = tags
+      expect(post.tags.map(&:name)).to eq(tags.map(&:name))
     end
 
   end
